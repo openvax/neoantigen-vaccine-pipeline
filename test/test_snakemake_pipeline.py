5947394fa8af6ecbbcbc7a44180540cc074cc739
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2018-07-19 15:05:17 -0400

    rearranged the helper functions for threads and memory, added seq2hla rule

--- test/test_snakemake_pipeline.py
+++ test/test_snakemake_pipeline.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import 
+
 import glob
 from os import chdir, listdir
 from os.path import dirname, join

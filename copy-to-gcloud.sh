b8e7e4d5de78d519e9ef639d309f502632eb54de
Julia K <julia326@gmail.com>
2019-04-10 12:34:11 +0900

    Changing copy-to-gcloud.sh to expect destination folder with full subdirectories (instead of appending "/snake" to folder name)

--- copy-to-gcloud.sh
+++ copy-to-gcloud.sh
@@ -3,7 +3,7 @@
 # Pass trial and patient ID as an arg to this script, which will be used to copy to
 # a particular directory on gcloud.
 #
-# Example usage: ./copy-to-gcloud.sh pgv001-012 pgv001/pt012
+# Example usage: ./copy-to-gcloud.sh pgv001-012 pgv001/pt012/snake
 
 set -e
 
@@ -14,21 +14,21 @@ fi
 
 DIRNAME=$1
 FOLDER=$2
-gsutil -m cp $DIRNAME/normal_aligned_coordinate_sorted_dups_indelreal_bqsr.bam gs://$FOLDER/snake/normal.bam
-gsutil -m cp $DIRNAME/normal_aligned_coordinate_sorted_dups_indelreal_bqsr.bai gs://$FOLDER/snake/normal.bam.bai
-gsutil -m cp $DIRNAME/tumor_aligned_coordinate_sorted_dups_indelreal_bqsr.bam gs://$FOLDER/snake/tumor.bam
-gsutil -m cp $DIRNAME/tumor_aligned_coordinate_sorted_dups_indelreal_bqsr.bai gs://$FOLDER/snake/tumor.bam.bai
-gsutil -m cp $DIRNAME/rna_final_sorted.bam gs://$FOLDER/snake/rna.bam
-gsutil -m cp $DIRNAME/rna_final_sorted.bam.bai gs://$FOLDER/snake/rna.bam.bai
+gsutil -m cp $DIRNAME/normal_aligned_coordinate_sorted_dups_indelreal_bqsr.bam gs://$FOLDER/normal.bam
+gsutil -m cp $DIRNAME/normal_aligned_coordinate_sorted_dups_indelreal_bqsr.bai gs://$FOLDER/normal.bam.bai
+gsutil -m cp $DIRNAME/tumor_aligned_coordinate_sorted_dups_indelreal_bqsr.bam gs://$FOLDER/tumor.bam
+gsutil -m cp $DIRNAME/tumor_aligned_coordinate_sorted_dups_indelreal_bqsr.bai gs://$FOLDER/tumor.bam.bai
+gsutil -m cp $DIRNAME/rna_final_sorted.bam gs://$FOLDER/rna.bam
+gsutil -m cp $DIRNAME/rna_final_sorted.bam.bai gs://$FOLDER/rna.bam.bai
 
-gsutil -m cp $DIRNAME/mutect.vcf gs://$FOLDER/snake/mutect.vcf
-gsutil -m cp $DIRNAME/strelka.vcf gs://$FOLDER/snake/strelka.vcf
-gsutil -m cp $DIRNAME/vaccine-peptide-report* gs://$FOLDER/snake/
-gsutil -m cp $DIRNAME/all-passing-variants*.csv gs://$FOLDER/snake/
+gsutil -m cp $DIRNAME/mutect.vcf gs://$FOLDER/mutect.vcf
+gsutil -m cp $DIRNAME/strelka.vcf gs://$FOLDER/strelka.vcf
+gsutil -m cp $DIRNAME/vaccine-peptide-report* gs://$FOLDER/
+gsutil -m cp $DIRNAME/all-passing-variants*.csv gs://$FOLDER/
 
-gsutil -m cp -r $DIRNAME/fastqc-output/ gs://$FOLDER/snake/
+gsutil -m cp -r $DIRNAME/fastqc-output/ gs://$FOLDER/
 
 # may or may not exist
-gsutil -m cp $DIRNAME/mutect2.vcf gs://$FOLDER/snake/mutect2.vcf
+gsutil -m cp $DIRNAME/mutect2.vcf gs://$FOLDER/mutect2.vcf
 
 echo "Done"

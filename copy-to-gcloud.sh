c5bec6c18031e9d5ce4a5dacad534847c393d291
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2019-04-16 12:07:30 -0400

    added more filetypes to copy-to-gcloud.sh

--- copy-to-gcloud.sh
+++ copy-to-gcloud.sh
@@ -12,29 +12,47 @@ if [ $# -lt 2 ]; then
     exit 1;
 fi
 
-DIRNAME=$1
-FOLDER=$2
-gsutil -m cp $DIRNAME/normal_aligned_coordinate_sorted_dups_indelreal_bqsr.bam gs://$FOLDER/normal.bam
-gsutil -m cp $DIRNAME/normal_aligned_coordinate_sorted_dups_indelreal_bqsr.bai gs://$FOLDER/normal.bam.bai
-gsutil -m cp $DIRNAME/tumor_aligned_coordinate_sorted_dups_indelreal_bqsr.bam gs://$FOLDER/tumor.bam
-gsutil -m cp $DIRNAME/tumor_aligned_coordinate_sorted_dups_indelreal_bqsr.bai gs://$FOLDER/tumor.bam.bai
+LOCAL_DIRNAME=$1
+GCLOUD_PATH=$2
 
+# remove trailing slashes in arguments
+LOCAL_DIRNAME=${LOCAL_DIRNAME%/}
+GCLOUD_PATH=${GCLOUD_PATH%/}
+
+echo "Arguments:"
+echo "LOCAL_DIRNAME=$LOCAL_DIRNAME"
+echo "GCLOUD_PATH=$GCLOUD_PATH"
+
+
+echo "=== Copying FastQC files ===" 
+gsutil -m cp -r $LOCAL_DIRNAME/fastqc-output/ gs://$GCLOUD_PATH/
+
+echo "=== Copying Picard metrics ==="
+gsutil -m cp -R $LOCAL_DIRNAME/*metrics*.txt gs://$CLOUD_PATH/picard-metrics
+
+echo "=== Copying logs ==="
+gsutil -m cp -R $LOCAL_DIRNAME/logs/ gs://$GCLOUD_PATH/
+
+echo "=== Copying VCF files ==="
 for vcf in mutect.vcf mutect2.vcf strelka.vcf filtered_normal_germline_snps_indels.vcf filtered_covered_normal_germline_snps_indels.vcf
 do
-    if [ -f $DIRNAME/$vcf ]
+    if [ -f $LOCAL_DIRNAME/$vcf ]
     then
-        gsutil -m cp $DIRNAME/$vcf gs://$FOLDER/$vcf
+        gsutil -m cp $LOCAL_DIRNAME/$vcf gs://$GCLOUD_PATH/$vcf
     else   
-        echo "Skipping $DIRNAME/$vcf, does not exist"
+        echo "Skipping $LOCAL_DIRNAME/$vcf, does not exist"
     fi
 done
 
-gsutil -m cp -r $DIRNAME/fastqc-output/ gs://$FOLDER/
+echo "=== Copying Vaxrank output ==="
+gsutil -m cp $LOCAL_DIRNAME/vaccine-peptide-report* gs://$GCLOUD_PATH/
+gsutil -m cp $LOCAL_DIRNAME/all-passing-variants*.csv gs://$GCLOUD_PATH/
 
-gsutil -m cp $DIRNAME/rna_final_sorted.bam gs://$FOLDER/rna.bam
-gsutil -m cp $DIRNAME/rna_final_sorted.bam.bai gs://$FOLDER/rna.bam.bai
-
-gsutil -m cp $DIRNAME/vaccine-peptide-report* gs://$FOLDER/
-gsutil -m cp $DIRNAME/all-passing-variants*.csv gs://$FOLDER/
-
+echo "=== Copying BAM files ==="
+gsutil -m cp $LOCAL_DIRNAME/normal_aligned_coordinate_sorted_dups_indelreal_bqsr.bam gs://$GCLOUD_PATH/normal.bam
+gsutil -m cp $LOCAL_DIRNAME/normal_aligned_coordinate_sorted_dups_indelreal_bqsr.bai gs://$GCLOUD_PATH/normal.bam.bai
+gsutil -m cp $LOCAL_DIRNAME/tumor_aligned_coordinate_sorted_dups_indelreal_bqsr.bam gs://$GCLOUD_PATH/tumor.bam
+gsutil -m cp $LOCAL_DIRNAME/tumor_aligned_coordinate_sorted_dups_indelreal_bqsr.bai gs://$GCLOUD_PATH/tumor.bam.bai
+gsutil -m cp $LOCAL_DIRNAME/rna_final_sorted.bam gs://$GCLOUD_PATH/rna.bam
+gsutil -m cp $LOCAL_DIRNAME/rna_final_sorted.bam.bai gs://$GCLOUD_PATH/rna.bam.bai
 echo "Done."
